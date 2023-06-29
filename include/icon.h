43e657c534d2a7e6bce72515a1595eb1eb3febc5
mokhaleee <nkulyc@163.com>
2023-06-29 20:06:33 +0800

    header nameing

--- include/icon.h
+++ include/icon.h
@@ -13,13 +13,17 @@ struct IconSt {
 extern u8 const Img_Icons[];
 extern u16 const Pal_Icons[];
 
-extern struct IconSt sIconStTable[MAX_ICON_COUNT];
-extern u8 sIconDisplayList[MAX_ICON_DISPLAY_COUNT];
+extern struct IconSt IconStTable[MAX_ICON_COUNT];
+extern u8 IconDisplayList[MAX_ICON_DISPLAY_COUNT];
 
 void InitIcons(void);
 void ClearIcons(void);
 void ApplyIconPalettes(int palid);
 void ApplyIconPalette(int num, int palid);
-void PutIcon(u16 * tm, int icon, int tileref);
+int CountActiveIcons(void);
+u16 IconSlot2Chr(int num);
+int GetNewIconSlot(int icon);
+int GetIconChr(int icon);
+void PutIcon(u16 *tm, int icon, int tileref);
 void ClearIcon(int icon);
 void PutIconObjImg(int icon, int chr);
