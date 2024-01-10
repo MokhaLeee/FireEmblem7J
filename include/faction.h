4d053fd05396082a05a09f07e60ed2ca22e1ed64
SetiQyu <leo.lundberg99@gmail.com>
2024-01-10 21:40:07 +0100

    faction.s fully decompiled

--- include/faction.h
+++ include/faction.h
@@ -3,3 +3,8 @@
 #include "global.h"
 
 int CountFactionMoveableUnits(int faction);
+int CountFactionUnitsWithoutFlags(int faction, int prohibited_flags);
+bool AreUnitIdsAllied(int uidA, int uidB);
+bool AreUnitIdsSameFaction(int uidA, int uidB);
+int GetActiveFactionAlliance();
+int GetActiveFactionOpposingAlliance();
--- src/faction.c
+++ src/faction.c
@@ -3,7 +3,6 @@
 #include "unit.h"
 #include "faction.h"
 
-
 int CountFactionMoveableUnits(int faction) {
 
 	int i, count = 0;
@@ -30,4 +29,52 @@ int CountFactionMoveableUnits(int faction) {
 		count++;
 	}
 	return count;
+}
+
+int CountFactionUnitsWithoutFlags(int faction, int prohibited_flags) {
+
+	int i, count = 0;
+
+	for (i = faction + 1; i < faction + 0x40; ++i) {
+
+		struct Unit* unit = GetUnit(i);
+
+		if (!unit)
+			continue;
+
+		if (!unit->pCharacterData)
+			continue;
+
+		if (unit->state & prohibited_flags)
+			continue;
+
+		count++;
+	}
+	return count;
+}
+
+bool AreUnitIdsAllied(int uidA, int uidB) {
+	
+	uidA &= 0x80;
+	uidB &= 0x80;
+	
+	return uidA == uidB;
+}
+
+bool AreUnitIdsSameFaction(int uidA, int uidB) {
+
+	uidA &= 0xC0;
+	uidB &= 0xC0;
+
+	return uidA == uidB;
+}
+
+int GetActiveFactionAlliance() {
+
+	return gPlaySt.faction & 0x80;
+}
+
+int GetActiveFactionOpposingAlliance() {
+	
+	return (gPlaySt.faction & 0x80) ^ 0x80;
 }
