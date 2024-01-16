e0d376e51389fc3fc5051abae50d23bee6f9a287
MokhaLeee <nkulyc@163.com>
2024-01-16 04:05:27 +0000

    split sprite data

--- data/data_08BBFCA8.s
+++ data/data_08BBFCA8.s
@@ -4,16 +4,48 @@
 
 	.global Sprite_8x8
 Sprite_8x8:
-	.incbin "FireEmblem7J.base.gba", 0xBFF760, 0xBFF768 - 0xBFF760
+	.incbin "FireEmblem7J.base.gba", 0xBFF760, 8
 
 	.global Sprite_16x16
 Sprite_16x16:
-	.incbin "FireEmblem7J.base.gba", 0xBFF768, 0xBFF770 - 0xBFF768
+	.incbin "FireEmblem7J.base.gba", 0xBFF768, 8
 
 	.global Sprite_32x32
 Sprite_32x32:
-	.incbin "FireEmblem7J.base.gba", 0xBFF770, 0xBFF788 - 0xBFF770
+	.incbin "FireEmblem7J.base.gba", 0xBFF770, 8
+
+	.global Sprite_64x64
+Sprite_64x64:
+	.incbin "FireEmblem7J.base.gba", 0xBFF778, 8
+
+	.global Sprite_8x16
+Sprite_8x16:
+	.incbin "FireEmblem7J.base.gba", 0xBFF780, 8
 
 	.global Sprite_16x32
 Sprite_16x32:
-	.incbin "FireEmblem7J.base.gba", 0xBFF788, 0xC01744 - 0xBFF788
+	.incbin "FireEmblem7J.base.gba", 0xBFF788, 8
+
+	.global Sprite_32x64
+Sprite_32x64:
+	.incbin "FireEmblem7J.base.gba", 0xBFF790, 8
+
+	.global Sprite_16x8
+Sprite_16x8:
+	.incbin "FireEmblem7J.base.gba", 0xBFF798, 8
+
+	.global Sprite_32x16
+Sprite_32x16:
+	.incbin "FireEmblem7J.base.gba", 0xBFF7A0, 8
+
+	.global Sprite_64x32
+Sprite_64x32:
+	.incbin "FireEmblem7J.base.gba", 0xBFF7A8, 8
+
+	.global Sprite_32x8
+Sprite_32x8:
+	.incbin "FireEmblem7J.base.gba", 0xBFF7B0, 8
+
+	.global Sprite_8x32
+Sprite_8x32:
+	.incbin "FireEmblem7J.base.gba", 0xBFF7B8, 0xC01744 - 0xBFF7B8
