f86da1af11efb035860d7cf0c999cf2573c43f4d
minirop <minirop@hotmail.com>
2024-09-13 20:21:25 +0200

    decompiled sub_80091AC, sub_8009218, sub_800925C, sub_8009418, sub_800954C, sub_80095D4, sub_80095E4 missing symbol for sub_800968C functionally matching sub_80096E0

--- src/talk.c
+++ src/talk.c
@@ -14,12 +14,18 @@ void SetTalkFlag(int);
 void sub_800EC84(int);
 void sub_8007F68(int);
 int sub_8007F58(int);
-
+void TalkToggleInvertedPalette(int);
 void sub_8014FB0(void (*)(), int);
+void sub_800981C();
+void sub_80098A0(int x, int y, int width, int height);
+void sub_8009920(int a, int b, int c, int d);
+void sub_8009AA8(int a, int b, int c, int d);
+void sub_8009628();
 
 extern struct ProcCmd gUnk_08BFFE18[];
 extern struct ProcCmd gUnk_08BFFD3C;
 extern struct ProcCmd gUnk_08BFFD4C[];
+extern struct ProcCmd gUnk_08BFFCBC;
 extern u16 gUnk_08BFFDB6[];
 extern u16 gUnk_08BFFD9C[];
 extern struct ProcCmd ProcScr_Talk[];
@@ -66,12 +72,846 @@ struct TalkSt
     /* 3C */ int number;
     /* 40 */ char buf_number_str[0x20];
     /* 60 */ char buf_unk_str[0x20];
+    /* 80 */ char unk_80;
+    /* 81 */ char unk_81;
+    /* 82 */ char unk_82;
+    /* 83 */ char unk_83;
+};
+
+struct TalkChoiceEnt
+{
+    u16 msg;
+    Func onSwitch;
+};
+
+struct TalkChoiceProc
+{
+    /* 00 */ PROC_HEADER;
+
+    /* 2A */ short selectedChoice;
+    /* 2C */ short x_disp;
+    /* 2E */ short y_disp;
+    /* 30 */ int unused30;
+    /* 34 */ struct TalkChoiceEnt const * choices;
 };
 
 extern struct TalkSt * CONST_DATA sTalkSt;
 extern int sTalkChoiceResult;
 extern int CONST_DATA gUnk_08BFFD7C[];
 extern u8* CONST_DATA gUnk_0818F93C[];
+extern int CONST_DATA gUnk_08BFFD2C[];
+
+#if 0
+extern struct Text sTalkText[3];
+#endif
+
+void sub_80091AC(struct Proc * proc)
+{
+    TmFillRect(&gBg0Tm[((sTalkSt->y_text + 4) << 5) + sTalkSt->x_text],
+        sTalkSt->active_width-2, sTalkSt->lines*2, 0);
+
+    TalkBgSync(BG0_SYNC_BIT);
+
+    proc->unk64 = 0;
+
+    if (sTalkSt->line_active == 0)
+    {
+        proc->unk66 = 16;
+    }
+    else if (sTalkSt->line_active + 1 >= sTalkSt->lines)
+    {
+        proc->unk66 = sTalkSt->lines * 16;
+    }
+    else
+    {
+        proc->unk66 = (sTalkSt->line_active + 1) * 16;
+    }
+}
+
+void sub_8009218(struct Proc * proc)
+{
+    proc->unk64++;
+
+    SetBgOffset(0, 0, proc->unk64);
+
+    if (proc->unk64 >= proc->unk66)
+    {
+        SetBgOffset(0, 0, 0);
+        sub_8009628();
+
+        Proc_Break(proc);
+    }
+}
+
+void sub_800925C(struct TalkChoiceEnt const * choices, struct Text * text, u16 * tm, int defaultChoice, int color, struct Proc * parent)
+{
+    struct TalkChoiceProc * proc;
+
+    int x = Text_GetCursor(text) + 16;
+
+    Text_InsertDrawString(text, x,      color, DecodeMsg(choices[0].msg));
+    Text_InsertDrawString(text, x + 40, color, DecodeMsg(choices[1].msg));
+
+    PutText(text, tm);
+
+    TalkBgSync(BG0_SYNC_BIT);
+
+    proc = Proc_StartBlocking(&gUnk_08BFFCBC, parent);
+
+    proc->selectedChoice = defaultChoice;
+
+    proc->x_disp = (((tm - gBg0Tm) & 0x1F) * 8) - gDispIo.bg_off[0].x + x;
+    proc->y_disp = (((tm - gBg0Tm) / 0x20) * 8) - gDispIo.bg_off[0].y;
+
+    proc->choices = choices;
+
+    if (proc->choices[defaultChoice - 1].onSwitch)
+        proc->choices[defaultChoice - 1].onSwitch();
+}
+
+NAKEDFUNC
+void sub_8009310()
+{
+    asm("   .syntax unified\n\
+    push {r4, r5, lr}\n\
+    adds r4, r0, #0\n\
+    ldr r0, _0800933C @ =gpKeySt\n\
+    ldr r0, [r0]\n\
+    ldrh r1, [r0, #8]\n\
+    movs r0, #2\n\
+    ands r0, r1\n\
+    cmp r0, #0\n\
+    beq _0800934C\n\
+    ldr r0, _08009340 @ =gPlaySt\n\
+    adds r0, #0x41\n\
+    ldrb r0, [r0]\n\
+    lsls r0, r0, #0x1e\n\
+    cmp r0, #0\n\
+    blt _08009334\n\
+    ldr r0, _08009344 @ =0x0000038B\n\
+    bl m4aSongNumStart\n\
+_08009334:\n\
+    ldr r1, _08009348 @ =sTalkChoiceResult\n\
+    movs r0, #0\n\
+    b _0800936E\n\
+    .align 2, 0\n\
+_0800933C: .4byte gpKeySt\n\
+_08009340: .4byte gPlaySt\n\
+_08009344: .4byte 0x0000038B\n\
+_08009348: .4byte sTalkChoiceResult\n\
+_0800934C:\n\
+    movs r5, #1\n\
+    adds r0, r5, #0\n\
+    ands r0, r1\n\
+    cmp r0, #0\n\
+    beq _08009384\n\
+    ldr r0, _08009378 @ =gPlaySt\n\
+    adds r0, #0x41\n\
+    ldrb r0, [r0]\n\
+    lsls r0, r0, #0x1e\n\
+    cmp r0, #0\n\
+    blt _08009368\n\
+    ldr r0, _0800937C @ =0x0000038A\n\
+    bl m4aSongNumStart\n\
+_08009368:\n\
+    ldr r1, _08009380 @ =sTalkChoiceResult\n\
+    movs r2, #0x2a\n\
+    ldrsh r0, [r4, r2]\n\
+_0800936E:\n\
+    str r0, [r1]\n\
+    adds r0, r4, #0\n\
+    bl Proc_Break\n\
+    b _08009404\n\
+    .align 2, 0\n\
+_08009378: .4byte gPlaySt\n\
+_0800937C: .4byte 0x0000038A\n\
+_08009380: .4byte sTalkChoiceResult\n\
+_08009384:\n\
+    movs r0, #0x20\n\
+    ands r0, r1\n\
+    cmp r0, #0\n\
+    beq _080093B2\n\
+    ldrh r0, [r4, #0x2a]\n\
+    cmp r0, #2\n\
+    bne _080093B2\n\
+    ldr r0, _0800940C @ =gPlaySt\n\
+    adds r0, #0x41\n\
+    ldrb r0, [r0]\n\
+    lsls r0, r0, #0x1e\n\
+    cmp r0, #0\n\
+    blt _080093A4\n\
+    ldr r0, _08009410 @ =0x00000387\n\
+    bl m4aSongNumStart\n\
+_080093A4:\n\
+    strh r5, [r4, #0x2a]\n\
+    ldr r0, [r4, #0x34]\n\
+    ldr r0, [r0, #4]\n\
+    cmp r0, #0\n\
+    beq _080093B2\n\
+    bl _call_via_r0\n\
+_080093B2:\n\
+    ldr r0, _08009414 @ =gpKeySt\n\
+    ldr r1, [r0]\n\
+    movs r0, #0x10\n\
+    ldrh r1, [r1, #8]\n\
+    ands r0, r1\n\
+    cmp r0, #0\n\
+    beq _080093E8\n\
+    ldrh r1, [r4, #0x2a]\n\
+    cmp r1, #1\n\
+    bne _080093E8\n\
+    ldr r0, _0800940C @ =gPlaySt\n\
+    adds r0, #0x41\n\
+    ldrb r0, [r0]\n\
+    lsls r0, r0, #0x1e\n\
+    cmp r0, #0\n\
+    blt _080093D8\n\
+    ldr r0, _08009410 @ =0x00000387\n\
+    bl m4aSongNumStart\n\
+_080093D8:\n\
+    movs r0, #2\n\
+    strh r0, [r4, #0x2a]\n\
+    ldr r0, [r4, #0x34]\n\
+    ldr r0, [r0, #0xc]\n\
+    cmp r0, #0\n\
+    beq _080093E8\n\
+    bl _call_via_r0\n\
+_080093E8:\n\
+    movs r2, #0x2c\n\
+    ldrsh r0, [r4, r2]\n\
+    movs r1, #0x2a\n\
+    ldrsh r2, [r4, r1]\n\
+    subs r2, #1\n\
+    lsls r1, r2, #2\n\
+    adds r1, r1, r2\n\
+    lsls r1, r1, #3\n\
+    adds r0, r0, r1\n\
+    subs r0, #4\n\
+    movs r2, #0x2e\n\
+    ldrsh r1, [r4, r2]\n\
+    bl PutUiHand\n\
+_08009404:\n\
+    pop {r4, r5}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_0800940C: .4byte gPlaySt\n\
+_08009410: .4byte 0x00000387\n\
+_08009414: .4byte gpKeySt\n\
+");
+}
+
+void sub_8009418(struct Proc * proc)
+{
+    TmFillRect_thm(&gBg0Tm[((sTalkSt->y_text + 4) << 5) + sTalkSt->x_text],
+        sTalkSt->active_width - 2, sTalkSt->lines * 2, 0);
+    TalkBgSync(1);
+    proc->unk64 = 0;
+}
+
+NAKEDFUNC
+void sub_8009458()
+{
+    asm("   .syntax unified\n\
+    push {r4, r5, r6, r7, lr}\n\
+    adds r7, r0, #0\n\
+    adds r4, r7, #0\n\
+    adds r4, #0x64\n\
+    ldrh r0, [r4]\n\
+    adds r0, #1\n\
+    strh r0, [r4]\n\
+    ldrh r2, [r4]\n\
+    movs r0, #0\n\
+    movs r1, #0\n\
+    bl SetBgOffset\n\
+    movs r1, #0\n\
+    ldrsh r0, [r4, r1]\n\
+    cmp r0, #0xf\n\
+    ble _08009538\n\
+    ldr r4, _08009540 @ =sTalkSt\n\
+    ldr r1, [r4]\n\
+    ldrb r0, [r1, #9]\n\
+    subs r0, #1\n\
+    strb r0, [r1, #9]\n\
+    ldr r1, [r4]\n\
+    ldrb r0, [r1, #0xb]\n\
+    adds r0, #1\n\
+    strb r0, [r1, #0xb]\n\
+    movs r0, #0\n\
+    movs r1, #0\n\
+    movs r2, #0\n\
+    bl SetBgOffset\n\
+    movs r5, #0\n\
+    ldr r0, [r4]\n\
+    ldrb r0, [r0, #0xa]\n\
+    subs r0, #1\n\
+    cmp r5, r0\n\
+    bge _080094D6\n\
+    adds r6, r4, #0\n\
+_080094A2:\n\
+    ldr r4, [r6]\n\
+    ldrb r2, [r4, #0xb]\n\
+    adds r0, r2, r5\n\
+    ldrb r1, [r4, #0xa]\n\
+    bl __modsi3\n\
+    lsls r0, r0, #3\n\
+    ldr r1, _08009544 @ =0x030000C8\n\
+    adds r0, r0, r1\n\
+    lsls r1, r5, #1\n\
+    ldrb r2, [r4, #0xd]\n\
+    adds r1, r2, r1\n\
+    lsls r1, r1, #5\n\
+    ldrb r4, [r4, #0xc]\n\
+    adds r1, r4, r1\n\
+    lsls r1, r1, #1\n\
+    ldr r2, _08009548 @ =gBg0Tm\n\
+    adds r1, r1, r2\n\
+    bl PutText\n\
+    adds r5, #1\n\
+    ldr r0, [r6]\n\
+    ldrb r0, [r0, #0xa]\n\
+    subs r0, #1\n\
+    cmp r5, r0\n\
+    blt _080094A2\n\
+_080094D6:\n\
+    ldr r4, _08009540 @ =sTalkSt\n\
+    ldr r2, [r4]\n\
+    ldrb r0, [r2, #0xa]\n\
+    subs r0, #1\n\
+    lsls r0, r0, #1\n\
+    ldrb r1, [r2, #0xd]\n\
+    adds r0, r1, r0\n\
+    lsls r0, r0, #5\n\
+    ldrb r1, [r2, #0xc]\n\
+    adds r0, r1, r0\n\
+    lsls r0, r0, #1\n\
+    ldr r1, _08009548 @ =gBg0Tm\n\
+    adds r0, r0, r1\n\
+    ldrb r1, [r2, #0xe]\n\
+    subs r1, #2\n\
+    movs r2, #2\n\
+    movs r3, #0\n\
+    bl TmFillRect_thm\n\
+    ldr r0, [r4]\n\
+    ldrb r1, [r0, #0xa]\n\
+    ldrb r0, [r0, #0xb]\n\
+    subs r0, #1\n\
+    adds r0, r1, r0\n\
+    bl __modsi3\n\
+    lsls r0, r0, #3\n\
+    ldr r5, _08009544 @ =0x030000C8\n\
+    adds r0, r0, r5\n\
+    bl ClearText\n\
+    ldr r4, [r4]\n\
+    ldrb r1, [r4, #0xa]\n\
+    ldrb r0, [r4, #0xb]\n\
+    subs r0, #1\n\
+    adds r0, r1, r0\n\
+    bl __modsi3\n\
+    lsls r0, r0, #3\n\
+    adds r0, r0, r5\n\
+    ldrb r1, [r4, #8]\n\
+    bl Text_SetColor\n\
+    movs r0, #1\n\
+    bl TalkBgSync\n\
+    adds r0, r7, #0\n\
+    bl Proc_Break\n\
+_08009538:\n\
+    pop {r4, r5, r6, r7}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_08009540: .4byte sTalkSt\n\
+_08009544: .4byte 0x030000C8\n\
+_08009548: .4byte gBg0Tm\n\
+");
+}
+
+void sub_800954C(struct Proc * proc)
+{
+    CleanTalkObjects(0x200, 0x19, 0x44444444, proc);
+}
+
+NAKEDFUNC
+void sub_8009564()
+{
+    asm("   .syntax unified\n\
+    push {r4, lr}\n\
+    ldr r0, _08009590 @ =sTalkSt\n\
+    ldr r1, [r0]\n\
+    ldrb r0, [r1, #9]\n\
+    subs r0, #1\n\
+    strb r0, [r1, #9]\n\
+    ldr r4, _08009594 @ =0x030000D0\n\
+    adds r0, r4, #0\n\
+    bl SpriteText_DrawBackground\n\
+    adds r0, r4, #0\n\
+    movs r1, #6\n\
+    bl Text_SetColor\n\
+    adds r0, r4, #0\n\
+    movs r1, #4\n\
+    bl Text_SetCursor\n\
+    pop {r4}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_08009590: .4byte sTalkSt\n\
+_08009594: .4byte 0x030000D0\n\
+");
+}
+
+NAKEDFUNC
+void sub_8009598()
+{
+    asm("   .syntax unified\n\
+    push {r4, r5, lr}\n\
+    ldr r0, _080095CC @ =sTalkSt\n\
+    ldr r1, [r0]\n\
+    movs r0, #0\n\
+    strb r0, [r1, #9]\n\
+    movs r5, #0\n\
+_080095A4:\n\
+    lsls r4, r5, #3\n\
+    ldr r0, _080095D0 @ =0x030000C8\n\
+    adds r4, r4, r0\n\
+    adds r0, r4, #0\n\
+    bl SpriteText_DrawBackground\n\
+    adds r0, r4, #0\n\
+    movs r1, #6\n\
+    bl Text_SetColor\n\
+    adds r0, r4, #0\n\
+    movs r1, #4\n\
+    bl Text_SetCursor\n\
+    adds r5, #1\n\
+    cmp r5, #1\n\
+    ble _080095A4\n\
+    pop {r4, r5}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_080095CC: .4byte sTalkSt\n\
+_080095D0: .4byte 0x030000C8\n\
+");
+}
+
+int sub_80095D4(int cmd)
+{
+    return gUnk_08BFFD2C[cmd - 4];
+}
+
+void sub_80095E4()
+{
+    sTalkSt->speak_talk_face = -1;
+    TmFill(gBg1Tm, 0);
+    TalkBgSync(2);
+    sub_8009628();
+    SetWinEnable(0, 0, 0);
+}
+
+NAKEDFUNC
+void sub_8009628()
+{
+    asm("   .syntax unified\n\
+    push {r4, r5, r6, lr}\n\
+    ldr r0, _08009680 @ =gBg0Tm\n\
+    movs r1, #0\n\
+    bl TmFill\n\
+    movs r0, #1\n\
+    bl TalkBgSync\n\
+    ldr r2, _08009684 @ =sTalkSt\n\
+    ldr r0, [r2]\n\
+    movs r1, #0\n\
+    strb r1, [r0, #9]\n\
+    ldr r0, [r2]\n\
+    adds r0, #0x82\n\
+    strb r1, [r0]\n\
+    ldr r0, [r2]\n\
+    strb r1, [r0, #0x15]\n\
+    ldr r0, [r2]\n\
+    strb r1, [r0, #0xb]\n\
+    movs r5, #0\n\
+    ldr r0, [r2]\n\
+    ldrb r0, [r0, #0xa]\n\
+    cmp r5, r0\n\
+    bge _0800967A\n\
+    adds r6, r2, #0\n\
+_0800965A:\n\
+    lsls r4, r5, #3\n\
+    ldr r0, _08009688 @ =0x030000C8\n\
+    adds r4, r4, r0\n\
+    adds r0, r4, #0\n\
+    bl ClearText\n\
+    ldr r0, [r6]\n\
+    ldrb r1, [r0, #8]\n\
+    adds r0, r4, #0\n\
+    bl Text_SetColor\n\
+    adds r5, #1\n\
+    ldr r0, [r6]\n\
+    ldrb r0, [r0, #0xa]\n\
+    cmp r5, r0\n\
+    blt _0800965A\n\
+_0800967A:\n\
+    pop {r4, r5, r6}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_08009680: .4byte gBg0Tm\n\
+_08009684: .4byte sTalkSt\n\
+_08009688: .4byte 0x030000C8\n\
+");
+}
+
+#if 0
+
+void sub_800968C()
+{
+    int i;
+
+    sTalkSt->line_active = FALSE;
+    sTalkSt->unk_82 = 0;
+    sTalkSt->put_lines = 0;
+    sTalkSt->top_text_num = 0;
+
+    for (i = 0; i < sTalkSt->lines; i++)
+    {
+        ClearText(sTalkText + i);
+        Text_SetColor(sTalkText + i, sTalkSt->print_color);
+    }
+}
+
+#else
+
+NAKEDFUNC
+void sub_800968C()
+{
+    asm("   .syntax unified\n\
+    push {r4, r5, r6, lr}\n\
+    ldr r2, _080096D8 @ =sTalkSt\n\
+    ldr r0, [r2]\n\
+    movs r1, #0\n\
+    strb r1, [r0, #9]\n\
+    ldr r0, [r2]\n\
+    adds r0, #0x82\n\
+    strb r1, [r0]\n\
+    ldr r0, [r2]\n\
+    strb r1, [r0, #0x15]\n\
+    ldr r0, [r2]\n\
+    strb r1, [r0, #0xb]\n\
+    movs r5, #0\n\
+    ldr r0, [r2]\n\
+    ldrb r0, [r0, #0xa]\n\
+    cmp r5, r0\n\
+    bge _080096D0\n\
+    adds r6, r2, #0\n\
+_080096B0:\n\
+    lsls r4, r5, #3\n\
+    ldr r0, _080096DC @ =0x030000C8\n\
+    adds r4, r4, r0\n\
+    adds r0, r4, #0\n\
+    bl ClearText\n\
+    ldr r0, [r6]\n\
+    ldrb r1, [r0, #8]\n\
+    adds r0, r4, #0\n\
+    bl Text_SetColor\n\
+    adds r5, #1\n\
+    ldr r0, [r6]\n\
+    ldrb r0, [r0, #0xa]\n\
+    cmp r5, r0\n\
+    blt _080096B0\n\
+_080096D0:\n\
+    pop {r4, r5, r6}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_080096D8: .4byte sTalkSt\n\
+_080096DC: .4byte 0x030000C8\n\
+");
+}
+
+#endif
+
+#if NONMATCHING
+
+/*
+what is this???
+ldr r1, [sp, #4]
+str r1, [sp]
+*/
+
+void sub_80096E0(int xAnchor, int yAnchor, int width, int height)
+{
+    int xTail;
+    int kind;
+    int tmp;
+    int x, y;
+
+    xTail = 0;
+
+    x = 0;
+    y = 0;
+
+    TmFill(gBg1Tm, 0);
+
+    if (xAnchor < 16)
+        kind = 0;
+    else
+        kind = 1;
+
+    if (IsBattleDeamonActive())
+        kind += 2;
+
+    y = yAnchor - height + 1;
+
+    switch (kind)
+    {
+
+    case 0:
+        xTail = xAnchor + 3;
+
+        tmp = xTail - width / 2;
+
+        if (tmp <= 0)
+            x = 1;
+        else
+            x = tmp;
+
+        break;
+
+    case 1:
+        xTail = xAnchor - 5;
+
+        tmp = xTail + (width + 1) / 2;
+
+        if (tmp > 29)
+            x = 29 - width;
+        else
+            x = xTail - width / 2;
+
+        break;
+
+    case 2:
+        x = 9;
+        y = 14;
+
+        width = 20;
+        xTail = x - 1;
+        yAnchor = y + 2;
+
+        break;
+
+    case 3:
+        x = 1;
+        y = 14;
+
+        width = 20;
+        xTail = x + width - 1;
+        yAnchor = y + 2;
+
+        break;
+    }
+
+    sTalkSt->x_text = x + 1;
+    sTalkSt->y_text = y + 1;
+
+    sub_8009AA8(1, x, y, width);
+    
+    if (sTalkSt->unk_83 & 2)
+    {
+        TalkToggleInvertedPalette(sTalkSt->unk_83 & 1);
+
+        sTalkSt->unk_83 ^= 2;
+    }
+    
+    if (sTalkSt->unk_83 & 1)
+    {
+        sub_8009920(1, xTail, yAnchor, kind);
+    }
+
+    sub_80098A0(x, y, width, height);
+
+    sub_800981C();
+
+    TalkBgSync(BG1_SYNC_BIT);
+}
+
+#else
+
+NAKEDFUNC
+void sub_80096E0()
+{
+    asm("   .syntax unified\n\
+    push {r4, r5, r6, r7, lr}\n\
+    mov r7, sl\n\
+    mov r6, sb\n\
+    mov r5, r8\n\
+    push {r5, r6, r7}\n\
+    sub sp, #8\n\
+    adds r5, r0, #0\n\
+    mov sl, r1\n\
+    adds r4, r2, #0\n\
+    str r3, [sp, #4]\n\
+    movs r0, #0\n\
+    mov r8, r0\n\
+    movs r6, #0\n\
+    ldr r0, _08009730 @ =gBg1Tm\n\
+    movs r1, #0\n\
+    bl TmFill\n\
+    movs r7, #1\n\
+    cmp r5, #0xf\n\
+    bgt _0800970A\n\
+    movs r7, #0\n\
+_0800970A:\n\
+    bl IsBattleDeamonActive\n\
+    lsls r0, r0, #0x18\n\
+    cmp r0, #0\n\
+    beq _08009716\n\
+    adds r7, #2\n\
+_08009716:\n\
+    mov r1, sl\n\
+    ldr r2, [sp, #4]\n\
+    subs r0, r1, r2\n\
+    adds r0, #1\n\
+    mov sb, r0\n\
+    cmp r7, #1\n\
+    beq _08009752\n\
+    cmp r7, #1\n\
+    bgt _08009734\n\
+    cmp r7, #0\n\
+    beq _0800973E\n\
+    b _08009796\n\
+    .align 2, 0\n\
+_08009730: .4byte gBg1Tm\n\
+_08009734:\n\
+    cmp r7, #2\n\
+    beq _08009776\n\
+    cmp r7, #3\n\
+    beq _08009788\n\
+    b _08009796\n\
+_0800973E:\n\
+    adds r5, #3\n\
+    mov r8, r5\n\
+    lsrs r0, r4, #0x1f\n\
+    adds r0, r4, r0\n\
+    asrs r0, r0, #1\n\
+    subs r6, r5, r0\n\
+    cmp r6, #0\n\
+    bgt _08009796\n\
+    movs r6, #1\n\
+    b _08009796\n\
+_08009752:\n\
+    subs r5, #5\n\
+    mov r8, r5\n\
+    adds r0, r4, #1\n\
+    lsrs r1, r0, #0x1f\n\
+    adds r0, r0, r1\n\
+    asrs r0, r0, #1\n\
+    add r0, r8\n\
+    cmp r0, #0x1d\n\
+    ble _0800976A\n\
+    movs r0, #0x1d\n\
+    subs r6, r0, r4\n\
+    b _08009796\n\
+_0800976A:\n\
+    lsrs r0, r4, #0x1f\n\
+    adds r0, r4, r0\n\
+    asrs r0, r0, #1\n\
+    mov r1, r8\n\
+    subs r6, r1, r0\n\
+    b _08009796\n\
+_08009776:\n\
+    movs r6, #9\n\
+    movs r2, #0xe\n\
+    mov sb, r2\n\
+    movs r4, #0x14\n\
+    movs r0, #8\n\
+    mov r8, r0\n\
+    movs r1, #0x10\n\
+    mov sl, r1\n\
+    b _08009796\n\
+_08009788:\n\
+    movs r6, #1\n\
+    movs r2, #0xe\n\
+    mov sb, r2\n\
+    movs r4, #0x14\n\
+    mov r8, r4\n\
+    movs r0, #0x10\n\
+    mov sl, r0\n\
+_08009796:\n\
+    ldr r5, _08009818 @ =sTalkSt\n\
+    ldr r1, [r5]\n\
+    adds r0, r6, #1\n\
+    strb r0, [r1, #0xc]\n\
+    ldr r1, [r5]\n\
+    mov r0, sb\n\
+    adds r0, #1\n\
+    strb r0, [r1, #0xd]\n\
+    ldr r1, [sp, #4]\n\
+    str r1, [sp]\n\
+    movs r0, #1\n\
+    adds r1, r6, #0\n\
+    mov r2, sb\n\
+    adds r3, r4, #0\n\
+    bl sub_8009AA8\n\
+    ldr r0, [r5]\n\
+    adds r0, #0x83\n\
+    ldrb r1, [r0]\n\
+    movs r0, #2\n\
+    ands r0, r1\n\
+    cmp r0, #0\n\
+    beq _080097D8\n\
+    movs r0, #1\n\
+    ands r0, r1\n\
+    bl TalkToggleInvertedPalette\n\
+    ldr r1, [r5]\n\
+    adds r1, #0x83\n\
+    movs r0, #2\n\
+    ldrb r2, [r1]\n\
+    eors r0, r2\n\
+    strb r0, [r1]\n\
+_080097D8:\n\
+    ldr r1, [r5]\n\
+    adds r1, #0x83\n\
+    movs r0, #1\n\
+    ldrb r1, [r1]\n\
+    ands r0, r1\n\
+    cmp r0, #0\n\
+    bne _080097F2\n\
+    movs r0, #1\n\
+    mov r1, r8\n\
+    mov r2, sl\n\
+    adds r3, r7, #0\n\
+    bl sub_8009920\n\
+_080097F2:\n\
+    adds r0, r6, #0\n\
+    mov r1, sb\n\
+    adds r2, r4, #0\n\
+    ldr r3, [sp, #4]\n\
+    bl sub_80098A0\n\
+    bl sub_800981C\n\
+    movs r0, #2\n\
+    bl TalkBgSync\n\
+    add sp, #8\n\
+    pop {r3, r4, r5}\n\
+    mov r8, r3\n\
+    mov sb, r4\n\
+    mov sl, r5\n\
+    pop {r4, r5, r6, r7}\n\
+    pop {r0}\n\
+    bx r0\n\
+    .syntax divided\n\
+    .align 2, 0\n\
+_08009818: .4byte sTalkSt\n\
+");
+}
+
+#endif
+
 
 void sub_800981C()
 {
@@ -167,7 +1007,7 @@ void sub_80098A0(int x, int y, int width, int height)
 }
 
 NAKEDFUNC
-void sub_8009920()
+void sub_8009920(int a, int b, int c, int d)
 {
     asm("   .syntax unified\n\
     push {r4, r5, r6, lr}\n\
@@ -358,7 +1198,7 @@ _08009AA4: .4byte 0x00003818\n\
 }
 
 NAKEDFUNC
-void sub_8009AA8(int bg, u16 * a1, u16 * a2, int len)
+void sub_8009AA8(int a, int b, int c, int d)
 {
     asm("   .syntax unified\n\
     push {r4, r5, r6, r7, lr}\n\
@@ -544,7 +1384,6 @@ void sub_8009BE8(struct Proc* proc)
     }
 }
 
-void sub_80096E0(short, short, short, short);
 void sub_8009C58(struct Proc* proc)
 {
     sub_80096E0(proc->unk64, proc->unk66, proc->unk68, proc->unk6A);
