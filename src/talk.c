9abdd24523ec3294f7ecd2b35450af74d9c65131
minirop <minirop@hotmail.com>
2024-09-16 00:50:53 +0200

    decompiled: TalkPutSpriteText_OnIdle, sub_8009FAC non-matching: sub_8009AA8

--- src/talk.c
+++ src/talk.c
@@ -91,8 +91,8 @@ void TalkToggleInvertedPalette(int);
 void sub_8014FB0(void (*)(), int);
 void sub_800981C();
 void sub_80098A0(int x, int y, int width, int height);
-void sub_8009920(int a, int b, int c, int d);
-void sub_8009AA8(int a, int b, int c, int d);
+void sub_8009920(int id, int xTail, int yAnchor, int kind);
+void sub_8009AA8(int id, int a, int b, int c, int d);
 void sub_8009628();
 int sub_8009D94(int talk_face);
 int sub_8008DC8(int x);
@@ -113,7 +113,7 @@ void sub_8007C48(struct FaceProc*, int);
 void sub_8007F44(int);
 void sub_800925C(struct TalkChoiceEnt const * choices, struct Text * text, u16 * tm, int defaultChoice, int color, struct Proc * parent);
 void LockTalk(ProcPtr proc);
-int sub_80149EC(int number, char * buf);
+u8 sub_80149EC(int number, char * buf);
 void sub_8008E58(int talkFaceFrom, int talkFaceTo);
 void sub_8009D0C(int talk_face, struct Proc* parent);
 void SetTalkFaceMouthMove(int face);
@@ -127,6 +127,7 @@ ProcPtr StartTalkExt(int x, int y, char const * str, ProcPtr parent);
 void sub_8007DF4();
 void sub_8007C64();
 void TryUnlockProc(ProcPtr proc);
+const char* sub_802EBB0();
 
 #define TALK_TEXT_BY_LINE(line) (sTalkText + ((line) + sTalkSt->top_text_num) % sTalkSt->lines)
 
@@ -2537,7 +2538,7 @@ void sub_80096E0(int xAnchor, int yAnchor, int width, int height)
     sTalkSt->x_text = x + 1;
     sTalkSt->y_text = y + 1;
 
-    sub_8009AA8(1, x, y, width);
+    sub_8009AA8(1, x, y, width, height);
     
     if (sTalkSt->unk_83 & 2)
     {
@@ -3027,8 +3028,46 @@ _08009AA4: .4byte 0x00003818\n\
 ");
 }
 
+#if NONMATCHING
+
+// adds r0, r1, r6
+// instead of
+// adds r0, r6, r1
+
+void sub_8009AA8(int id, int x, int y, int width, int height)
+{
+    int i, j;
+
+    u16 * tilemap = GetBgTilemap(id);
+    width--;
+    height--;
+
+    for (i = x; i < x + width; ++i)
+    {
+        tilemap[TM_OFFSET(i, y)]          = TILEREF(0x10 + 1, BGPAL_TALK_BUBBLE);
+        tilemap[TM_OFFSET(i, y + height)] = TILEREF(0x10 + 1, BGPAL_TALK_BUBBLE) + TILE_VFLIP;
+    }
+
+    for (i = y; i < y + height; ++i)
+    {
+        tilemap[TM_OFFSET(x, i)]         = TILEREF(0x10 + 2, BGPAL_TALK_BUBBLE);
+        tilemap[TM_OFFSET(x + width, i)] = TILEREF(0x10 + 2, BGPAL_TALK_BUBBLE) + TILE_HFLIP;
+    }
+
+    for (i = x + 1; i < x + width; ++i)
+        for (j = y + 1; j < y + height; ++j)
+            tilemap[TM_OFFSET(i, j)] = TILEREF(0x10 + 3, BGPAL_TALK_BUBBLE);
+
+    tilemap[TM_OFFSET(x,         y)]          = TILEREF(0x10 + 0, BGPAL_TALK_BUBBLE);
+    tilemap[TM_OFFSET(x + width, y)]          = TILEREF(0x10 + 0, BGPAL_TALK_BUBBLE) + TILE_HFLIP;
+    tilemap[TM_OFFSET(x,         y + height)] = TILEREF(0x10 + 0, BGPAL_TALK_BUBBLE) + TILE_VFLIP;
+    tilemap[TM_OFFSET(x + width, y + height)] = TILEREF(0x10 + 0, BGPAL_TALK_BUBBLE) + TILE_HFLIP + TILE_VFLIP;
+}
+
+#else
+
 NAKEDFUNC
-void sub_8009AA8(int a, int b, int c, int d)
+void sub_8009AA8(int id, int x, int y, int width, int height)
 {
     asm("   .syntax unified\n\
     push {r4, r5, r6, r7, lr}\n\
@@ -3192,6 +3231,8 @@ _08009BE0: .4byte 0x00003C10\n\
 ");
 }
 
+#endif
+
 void nullsub_25()
 {
 }
@@ -3290,6 +3331,8 @@ int sub_8009D94(int talk_face)
 }
 
 #if NONMATCHING
+
+// 'cause gUnk_0818F958
 
 void SetTalkFaceDisp(int talk_face, int faceDisp)
 {
@@ -3398,7 +3441,7 @@ void SetTalkUnkStr(char* buffer)
 }
 
 NAKEDFUNC
-void PrintStringToTexts(struct Text ** texts, char const * str, int x, int y)
+void PrintStringToTexts(struct Text ** texts, char const * str, u16 * buffer, int x, int y)
 {
     asm("   .syntax unified\n\
     push {r4, r5, r6, r7, lr}\n\
@@ -3461,72 +3504,15 @@ _08009EF8:\n\
 ");
 }
 
-NAKEDFUNC
 void TalkPutSpriteText_OnIdle(struct Proc * proc)
 {
-    asm("   .syntax unified\n\
-    push {r4, r5, r6, r7, lr}\n\
-    mov r7, sl\n\
-    mov r6, sb\n\
-    mov r5, r8\n\
-    push {r5, r6, r7}\n\
-    sub sp, #4\n\
-    adds r7, r0, #0\n\
-    ldr r1, [r7, #0x2c]\n\
-    ldr r2, [r7, #0x30]\n\
-    ldr r0, _08009F7C @ =gUnk_08BFFDB6\n\
-    mov ip, r0\n\
-    movs r3, #0x52\n\
-    adds r3, r3, r7\n\
-    mov sb, r3\n\
-    ldr r4, _08009F80 @ =0x000003FF\n\
-    mov sl, r4\n\
-    ldrh r6, [r3]\n\
-    ands r4, r6\n\
-    movs r0, #0x64\n\
-    adds r0, r0, r7\n\
-    mov r8, r0\n\
-    movs r5, #0xf\n\
-    adds r0, r5, #0\n\
-    mov r3, r8\n\
-    ldrh r3, [r3]\n\
-    ands r0, r3\n\
-    lsls r0, r0, #0xc\n\
-    orrs r4, r0\n\
-    str r4, [sp]\n\
-    movs r0, #3\n\
-    mov r3, ip\n\
-    bl PutSprite\n\
-    ldr r1, [r7, #0x2c]\n\
-    ldr r2, [r7, #0x30]\n\
-    ldr r3, _08009F84 @ =gUnk_08BFFD9C\n\
-    mov r6, sl\n\
-    mov r4, sb\n\
-    ldrh r4, [r4]\n\
-    ands r6, r4\n\
-    ldr r0, _08009F88 @ =0x030000E8\n\
-    ldrh r0, [r0, #0x14]\n\
-    ands r5, r0\n\
-    lsls r5, r5, #0xc\n\
-    orrs r6, r5\n\
-    str r6, [sp]\n\
-    movs r0, #3\n\
-    bl PutSprite\n\
-    add sp, #4\n\
-    pop {r3, r4, r5}\n\
-    mov r8, r3\n\
-    mov sb, r4\n\
-    mov sl, r5\n\
-    pop {r4, r5, r6, r7}\n\
-    pop {r0}\n\
-    bx r0\n\
-    .syntax divided\n\
-    .align 2, 0\n\
-_08009F7C: .4byte gUnk_08BFFDB6\n\
-_08009F80: .4byte 0x000003FF\n\
-_08009F84: .4byte gUnk_08BFFD9C\n\
-_08009F88: .4byte 0x030000E8\n\
-");
+    PutSprite(3,
+        proc->x, proc->y, gUnk_08BFFDB6,
+        OAM2_CHR(proc->unk52) | OAM2_PAL(proc->unk64));
+
+    PutSprite(3,
+        proc->x, proc->y, gUnk_08BFFD9C,
+        OAM2_CHR(proc->unk52) | OAM2_PAL(sTalkFont.palid));
 }
 
 void sub_8009F8C()
@@ -3539,354 +3525,206 @@ void sub_8009F98()
     sub_8014FB0(&sub_8009F8C, 1);
 }
 
-NAKEDFUNC
 int sub_8009FAC(char const * str, bool isBubbleOpen)
 {
-    asm("\n\
-    .syntax unified\n\
-    push {r4, r5, r6, r7, lr}\n\
-    mov r7, sb\n\
-    mov r6, r8\n\
-    push {r6, r7}\n\
-    sub sp, #0x24\n\
-    adds r4, r0, #0\n\
-    lsls r1, r1, #0x18\n\
-    lsrs r1, r1, #0x18\n\
-    mov r8, r1\n\
-    ldr r0, _08009FE0 @ =sTalkSt\n\
-    ldr r0, [r0]\n\
-    movs r1, #0xf\n\
-    ldrsb r1, [r0, r1]\n\
-    mov sb, r1\n\
-    ldrb r5, [r0, #0x11]\n\
-    movs r6, #0\n\
-    movs r7, #0x18\n\
-_08009FCE:\n\
-    ldrb r0, [r4]\n\
-    cmp r0, #0x81\n\
-    bls _08009FD6\n\
-    b _0800A342\n\
-_08009FD6:\n\
-    lsls r0, r0, #2\n\
-    ldr r1, _08009FE4 @ =_08009FE8\n\
-    adds r0, r0, r1\n\
-    ldr r0, [r0]\n\
-    mov pc, r0\n\
-    .align 2, 0\n\
-_08009FE0: .4byte sTalkSt\n\
-_08009FE4: .4byte _08009FE8\n\
-_08009FE8: @ jump table\n\
-    .4byte _0800A1F0 @ case 0\n\
-    .4byte _0800A1FA @ case 1\n\
-    .4byte _0800A1FA @ case 2\n\
-    .4byte _0800A206 @ case 3\n\
-    .4byte _0800A202 @ case 4\n\
-    .4byte _0800A202 @ case 5\n\
-    .4byte _0800A202 @ case 6\n\
-    .4byte _0800A202 @ case 7\n\
-    .4byte _0800A20A @ case 8\n\
-    .4byte _0800A20A @ case 9\n\
-    .4byte _0800A20A @ case 10\n\
-    .4byte _0800A20A @ case 11\n\
-    .4byte _0800A20A @ case 12\n\
-    .4byte _0800A20A @ case 13\n\
-    .4byte _0800A20A @ case 14\n\
-    .4byte _0800A20A @ case 15\n\
-    .4byte _0800A210 @ case 16\n\
-    .4byte _0800A22E @ case 17\n\
-    .4byte _0800A234 @ case 18\n\
-    .4byte _0800A234 @ case 19\n\
-    .4byte _0800A234 @ case 20\n\
-    .4byte _0800A234 @ case 21\n\
-    .4byte _0800A202 @ case 22\n\
-    .4byte _0800A202 @ case 23\n\
-    .4byte _0800A23C @ case 24\n\
-    .4byte _0800A23C @ case 25\n\
-    .4byte _0800A23C @ case 26\n\
-    .4byte _0800A23C @ case 27\n\
-    .4byte _0800A202 @ case 28\n\
-    .4byte _0800A342 @ case 29\n\
-    .4byte _0800A342 @ case 30\n\
-    .4byte _0800A342 @ case 31\n\
-    .4byte _0800A342 @ case 32\n\
-    .4byte _0800A342 @ case 33\n\
-    .4byte _0800A342 @ case 34\n\
-    .4byte _0800A342 @ case 35\n\
-    .4byte _0800A342 @ case 36\n\
-    .4byte _0800A342 @ case 37\n\
-    .4byte _0800A342 @ case 38\n\
-    .4byte _0800A342 @ case 39\n\
-    .4byte _0800A342 @ case 40\n\
-    .4byte _0800A342 @ case 41\n\
-    .4byte _0800A342 @ case 42\n\
-    .4byte _0800A342 @ case 43\n\
-    .4byte _0800A342 @ case 44\n\
-    .4byte _0800A342 @ case 45\n\
-    .4byte _0800A342 @ case 46\n\
-    .4byte _0800A342 @ case 47\n\
-    .4byte _0800A342 @ case 48\n\
-    .4byte _0800A342 @ case 49\n\
-    .4byte _0800A342 @ case 50\n\
-    .4byte _0800A342 @ case 51\n\
-    .4byte _0800A342 @ case 52\n\
-    .4byte _0800A342 @ case 53\n\
-    .4byte _0800A342 @ case 54\n\
-    .4byte _0800A342 @ case 55\n\
-    .4byte _0800A342 @ case 56\n\
-    .4byte _0800A342 @ case 57\n\
-    .4byte _0800A342 @ case 58\n\
-    .4byte _0800A342 @ case 59\n\
-    .4byte _0800A342 @ case 60\n\
-    .4byte _0800A342 @ case 61\n\
-    .4byte _0800A342 @ case 62\n\
-    .4byte _0800A342 @ case 63\n\
-    .4byte _0800A342 @ case 64\n\
-    .4byte _0800A342 @ case 65\n\
-    .4byte _0800A342 @ case 66\n\
-    .4byte _0800A342 @ case 67\n\
-    .4byte _0800A342 @ case 68\n\
-    .4byte _0800A342 @ case 69\n\
-    .4byte _0800A342 @ case 70\n\
-    .4byte _0800A342 @ case 71\n\
-    .4byte _0800A342 @ case 72\n\
-    .4byte _0800A342 @ case 73\n\
-    .4byte _0800A342 @ case 74\n\
-    .4byte _0800A342 @ case 75\n\
-    .4byte _0800A342 @ case 76\n\
-    .4byte _0800A342 @ case 77\n\
-    .4byte _0800A342 @ case 78\n\
-    .4byte _0800A342 @ case 79\n\
-    .4byte _0800A342 @ case 80\n\
-    .4byte _0800A342 @ case 81\n\
-    .4byte _0800A342 @ case 82\n\
-    .4byte _0800A342 @ case 83\n\
-    .4byte _0800A342 @ case 84\n\
-    .4byte _0800A342 @ case 85\n\
-    .4byte _0800A342 @ case 86\n\
-    .4byte _0800A342 @ case 87\n\
-    .4byte _0800A342 @ case 88\n\
-    .4byte _0800A342 @ case 89\n\
-    .4byte _0800A342 @ case 90\n\
-    .4byte _0800A342 @ case 91\n\
-    .4byte _0800A342 @ case 92\n\
-    .4byte _0800A342 @ case 93\n\
-    .4byte _0800A342 @ case 94\n\
-    .4byte _0800A342 @ case 95\n\
-    .4byte _0800A342 @ case 96\n\
-    .4byte _0800A342 @ case 97\n\
-    .4byte _0800A342 @ case 98\n\
-    .4byte _0800A342 @ case 99\n\
-    .4byte _0800A342 @ case 100\n\
-    .4byte _0800A342 @ case 101\n\
-    .4byte _0800A342 @ case 102\n\
-    .4byte _0800A342 @ case 103\n\
-    .4byte _0800A342 @ case 104\n\
-    .4byte _0800A342 @ case 105\n\
-    .4byte _0800A342 @ case 106\n\
-    .4byte _0800A342 @ case 107\n\
-    .4byte _0800A342 @ case 108\n\
-    .4byte _0800A342 @ case 109\n\
-    .4byte _0800A342 @ case 110\n\
-    .4byte _0800A342 @ case 111\n\
-    .4byte _0800A342 @ case 112\n\
-    .4byte _0800A342 @ case 113\n\
-    .4byte _0800A342 @ case 114\n\
-    .4byte _0800A342 @ case 115\n\
-    .4byte _0800A342 @ case 116\n\
-    .4byte _0800A342 @ case 117\n\
-    .4byte _0800A342 @ case 118\n\
-    .4byte _0800A342 @ case 119\n\
-    .4byte _0800A342 @ case 120\n\
-    .4byte _0800A342 @ case 121\n\
-    .4byte _0800A342 @ case 122\n\
-    .4byte _0800A342 @ case 123\n\
-    .4byte _0800A342 @ case 124\n\
-    .4byte _0800A342 @ case 125\n\
-    .4byte _0800A342 @ case 126\n\
-    .4byte _0800A342 @ case 127\n\
-    .4byte _0800A240 @ case 128\n\
-    .4byte _0800A336 @ case 129\n\
-_0800A1F0:\n\
-    cmp r6, r7\n\
-    bgt _0800A1F6\n\
-    b _0800A368\n\
-_0800A1F6:\n\
-    adds r7, r6, #0\n\
-    b _0800A368\n\
-_0800A1FA:\n\
-    cmp r6, r7\n\
-    ble _0800A200\n\
-    adds r7, r6, #0\n\
-_0800A200:\n\
-    movs r6, #0\n\
-_0800A202:\n\
-    adds r4, #1\n\
-    b _08009FCE\n\
-_0800A206:\n\
-    adds r6, #0xc\n\
-    b _0800A202\n\
-_0800A20A:\n\
-    ldrb r5, [r4]\n\
-    subs r5, #8\n\
-    b _0800A202\n\
-_0800A210:\n\
-    ldrb r0, [r4]\n\
-    cmp r0, #8\n\
-    bge _0800A218\n\
-    b _08009FCE\n\
-_0800A218:\n\
-    cmp r0, #0xf\n\
-    ble _0800A222\n\
-    cmp r0, #0x10\n\
-    beq _0800A22A\n\
-    b _08009FCE\n\
-_0800A222:\n\
-    adds r5, r0, #0\n\
-    subs r5, #8\n\
-    adds r4, #1\n\
-    b _0800A210\n\
-_0800A22A:\n\
-    adds r4, #3\n\
-    b _0800A210\n\
-_0800A22E:\n\
-    cmp r5, sb\n\
-    beq _0800A1F0\n\
-    b _0800A202\n\
-_0800A234:\n\
-    mov r2, r8\n\
-    cmp r2, #0\n\
-    beq _0800A1F0\n\
-    b _0800A202\n\
-_0800A23C:\n\
-    adds r6, #0x80\n\
-    b _0800A202\n\
-_0800A240:\n\
-    adds r4, #1\n\
-    ldrb r0, [r4]\n\
-    cmp r0, #0x25\n\
-    bls _0800A24A\n\
-    b _08009FCE\n\
-_0800A24A:\n\
-    lsls r0, r0, #2\n\
-    ldr r1, _0800A254 @ =_0800A258\n\
-    adds r0, r0, r1\n\
-    ldr r0, [r0]\n\
-    mov pc, r0\n\
-    .align 2, 0\n\
-_0800A254: .4byte _0800A258\n\
-_0800A258: @ jump table\n\
-    .4byte _0800A202 @ case 0\n\
-    .4byte _0800A202 @ case 1\n\
-    .4byte _0800A202 @ case 2\n\
-    .4byte _0800A202 @ case 3\n\
-    .4byte _0800A202 @ case 4\n\
-    .4byte _0800A2F0 @ case 5\n\
-    .4byte _0800A316 @ case 6\n\
-    .4byte _0800A202 @ case 7\n\
-    .4byte _0800A202 @ case 8\n\
-    .4byte _0800A202 @ case 9\n\
-    .4byte _0800A330 @ case 10\n\
-    .4byte _0800A330 @ case 11\n\
-    .4byte _0800A330 @ case 12\n\
-    .4byte _0800A330 @ case 13\n\
-    .4byte _0800A330 @ case 14\n\
-    .4byte _0800A330 @ case 15\n\
-    .4byte _0800A330 @ case 16\n\
-    .4byte _0800A330 @ case 17\n\
-    .4byte _08009FCE @ case 18\n\
-    .4byte _08009FCE @ case 19\n\
-    .4byte _08009FCE @ case 20\n\
-    .4byte _08009FCE @ case 21\n\
-    .4byte _0800A202 @ case 22\n\
-    .4byte _0800A202 @ case 23\n\
-    .4byte _0800A202 @ case 24\n\
-    .4byte _0800A202 @ case 25\n\
-    .4byte _0800A202 @ case 26\n\
-    .4byte _0800A202 @ case 27\n\
-    .4byte _0800A202 @ case 28\n\
-    .4byte _0800A202 @ case 29\n\
-    .4byte _0800A202 @ case 30\n\
-    .4byte _0800A202 @ case 31\n\
-    .4byte _0800A30C @ case 32\n\
-    .4byte _0800A202 @ case 33\n\
-    .4byte _08009FCE @ case 34\n\
-    .4byte _08009FCE @ case 35\n\
-    .4byte _0800A202 @ case 36\n\
-    .4byte _0800A202 @ case 37\n\
-_0800A2F0:\n\
-    ldr r0, _0800A308 @ =sTalkSt\n\
-    ldr r0, [r0]\n\
-    ldr r0, [r0, #0x3c]\n\
-    mov r1, sp\n\
-    bl sub_80149EC\n\
-    mov r0, r8\n\
-    lsls r1, r0, #0x18\n\
-    asrs r1, r1, #0x18\n\
-    mov r0, sp\n\
-    b _0800A322\n\
-    .align 2, 0\n\
-_0800A308: .4byte sTalkSt\n\
-_0800A30C:\n\
-    bl sub_802EBB0\n\
-    bl GetStringTextLen\n\
-    b _0800A326\n\
-_0800A316:\n\
-    ldr r0, _0800A32C @ =sTalkSt\n\
-    ldr r0, [r0]\n\
-    adds r0, #0x60\n\
-    mov r2, r8\n\
-    lsls r1, r2, #0x18\n\
-    asrs r1, r1, #0x18\n\
-_0800A322:\n\
-    bl sub_8009FAC\n\
-_0800A326:\n\
-    adds r6, r6, r0\n\
-    b _0800A202\n\
-    .align 2, 0\n\
-_0800A32C: .4byte sTalkSt\n\
-_0800A330:\n\
-    ldrb r5, [r4]\n\
-    subs r5, #0xa\n\
-    b _0800A202\n\
-_0800A336:\n\
-    ldrb r0, [r4, #1]\n\
-    cmp r0, #0x40\n\
-    bne _0800A342\n\
-    adds r4, #2\n\
-    adds r6, #6\n\
-    b _08009FCE\n\
-_0800A342:\n\
-    cmp r5, sb\n\
-    beq _0800A358\n\
-    cmp r5, #0xff\n\
-    beq _0800A358\n\
-    mov r1, r8\n\
-    cmp r1, #0\n\
-    beq _0800A352\n\
-    b _0800A1F0\n\
-_0800A352:\n\
-    movs r2, #1\n\
-    mov r8, r2\n\
-    mov sb, r5\n\
-_0800A358:\n\
-    add r1, sp, #0x20\n\
-    adds r0, r4, #0\n\
-    bl GetCharTextLen\n\
-    adds r4, r0, #0\n\
-    ldr r0, [sp, #0x20]\n\
-    adds r6, r6, r0\n\
-    b _08009FCE\n\
-_0800A368:\n\
-    adds r0, r7, #0\n\
-    add sp, #0x24\n\
-    pop {r3, r4}\n\
-    mov r8, r3\n\
-    mov sb, r4\n\
-    pop {r4, r5, r6, r7}\n\
-    pop {r1}\n\
-    bx r1\n\
-    .syntax divided\n\
-");
+    char buf[0x20];
+
+    int speakFace = sTalkSt->speak_talk_face;
+    int activeFace = sTalkSt->active_talk_face;
+
+    s32 chrlen;
+
+    int currentLineLen = 0;
+    int maxLineLen = 24;
+
+    for (;TRUE;)
+    {
+        switch (*str)
+        {
+
+        case 0x00: // end
+            if (currentLineLen > maxLineLen)
+                maxLineLen = currentLineLen;
+
+            currentLineLen = 0;
+
+            goto ret;
+
+        case 0x01 ... 0x02:
+            if (currentLineLen > maxLineLen)
+                maxLineLen = currentLineLen;
+
+            currentLineLen = 0;
+
+            str++;
+            break;
+
+        case 0x03:
+            currentLineLen += 12;
+            str++;
+            break;
+
+        case 0x04 ... 0x07:
+            str++;
+            break;
+
+        case 0x08 ... 0x0F:
+            activeFace = *str - 0x08;
+            str++;
+            break;
+
+        case 0x10:
+            for (;TRUE;)
+            {
+                switch (*str)
+                {
+
+                case 0x08 ... 0x0F:
+                    activeFace = *str - 0x08;
+                    str++;
+                    continue;
+
+                case 0x10:
+                    str++;
+                    str++;
+                    str++;
+
+                    continue;
+
+                }
+
+                break;
+            }
+
+            break;
+
+        case 0x11: 
+            if (activeFace == speakFace)
+            {
+                if (currentLineLen > maxLineLen)
+                    maxLineLen = currentLineLen;
+
+                currentLineLen = 0;
+
+                goto ret;
+            }
+
+            str++;
+            break;
+
+        case 0x12 ... 0x15:
+            if (!isBubbleOpen)
+            {
+                if (currentLineLen > maxLineLen)
+                    maxLineLen = currentLineLen;
+
+                currentLineLen = 0;
+
+                goto ret;
+            }
+
+            str++;
+            break;
+
+        case 0x16 ... 0x17:
+            str++;
+            break;
+
+        case 0x18 ... 0x1B: // choice
+            currentLineLen += 128;
+            str++;
+            break;
+
+        case 0x1C:
+            str++;
+            break;
+
+        case 0x80:
+            str++;
+
+            switch (*str)
+            {
+
+            case 0x00 ... 0x04:
+            case 0x07 ... 0x09:
+            case 0x16 ... 0x1F:
+            case 0x21:
+            case 0x24:
+            case 0x25:
+                str++;
+                break;
+
+            case 0x05:
+                sub_80149EC(sTalkSt->number, buf);
+                currentLineLen += sub_8009FAC(buf, isBubbleOpen);
+
+                str++;
+                break;
+
+            case 0x20:
+                currentLineLen += GetStringTextLen(sub_802EBB0());
+
+                str++;
+                break;
+
+            case 0x06:
+                currentLineLen += sub_8009FAC(sTalkSt->buf_unk_str, isBubbleOpen);
+
+                str++;
+                break;
+
+            case 0x0A ... 0x11:
+                activeFace = *str - 0x0A;
+                str++;
+                break;
+
+            case 0x12 ... 0x15:
+            case 0x22 ... 0x23:
+                break;
+
+            default:
+                break;
+
+            }
+
+            break;
+
+        case 0x81:
+            if (str[1] == 0x40)
+            {
+                str += 2;
+                currentLineLen += 6;
+                break;
+            }
+
+            // fallthrough
+
+        default:
+            if ((activeFace != speakFace) && (activeFace != TALK_FACE_NONE))
+            {
+                if (!isBubbleOpen)
+                {
+                    isBubbleOpen = TRUE;
+                    speakFace = activeFace;
+                }
+                else
+                {
+                    if (currentLineLen > maxLineLen)
+                        maxLineLen = currentLineLen;
+
+                    currentLineLen = 0;
+
+                    goto ret;
+                }
+            }
+
+            str = GetCharTextLen(str, &chrlen);
+            currentLineLen += chrlen;
+        }
+    }
+
+ret:
+    return maxLineLen;
 }
 
 bool sub_800A378()
