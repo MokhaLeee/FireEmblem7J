950d1bc63d2aa4d7d6125a53e403b09c03cfca9e
MokhaLeee <nkulyc@163.com>
2024-10-29 13:48:50 +0800

    Split spawn assain ap

--- data/EventCallfx_SpriteAnim_SpawnAssassin.s
+++ data/EventCallfx_SpriteAnim_SpawnAssassin.s
@@ -7,7 +7,3 @@ Img_EventSpriteAnim_SpawnAssassin:	@ 0x081C778C
 	.global Img_EventSpriteAnim_SpawnThief
 Img_EventSpriteAnim_SpawnThief:	@ 0x081C7D20
 	.incbin "FireEmblem7J.base.gba", 0x1C7D20, 0x1C82E0 - 0x1C7D20
-
-	.global ApConf_EventSpriteAnim_SpawnAssassin
-ApConf_EventSpriteAnim_SpawnAssassin:	@ 0x081C82E0
-	.incbin "FireEmblem7J.base.gba", 0x1C82E0, 0x1C83E8 - 0x1C82E0
---
+++ data/EventCallfx_SpriteAnim_SpawnAssassinAp.s
@@ -0,0 +1,5 @@
+	.section .rodata
+
+	.global ApConf_EventSpriteAnim_SpawnAssassin
+ApConf_EventSpriteAnim_SpawnAssassin:	@ 0x081C82E0
+	.incbin "FireEmblem7J.base.gba", 0x1C82E0, 0x1C83E8 - 0x1C82E0
