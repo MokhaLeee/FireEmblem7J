570c828f330fecc959a05e24fca97a481d076a42
Eebit <eight_bit_hero@yahoo.ca>
2025-05-21 18:29:18 -0400

    (prepscreen) Finish decompiling; add chapter constants

---
+++ include/constants/chapters.h
@@ -0,0 +1,53 @@
+#pragma once
+
+enum
+{
+    CHAPTER_00 = 0x00, // Prologue: A Girl from the Plains
+    CHAPTER_01 = 0x01, // Ch. 1: Footsteps of Fate
+    CHAPTER_02 = 0x02, // Ch. 2: Sword of Spirits
+    CHAPTER_03 = 0x03, // Ch. 3: Band of Mercenaries
+    CHAPTER_04 = 0x04, // Ch. 4: In Occupation's Shadow
+    CHAPTER_05 = 0x05, // Ch. 5: Beyond the Borders
+    CHAPTER_06 = 0x06, // Ch. 6: Blood of Pride
+    CHAPTER_07 = 0x07, // Ch. 7: Siblings Abroad
+    CHAPTER_08 = 0x08, // Ch. 7x: The Black Shadow
+    CHAPTER_09 = 0x09, // Ch. 8: Vortex of Strategy
+    CHAPTER_0A = 0x0A, // Ch. 9: A Grim Reunion
+    CHAPTER_0B = 0x0B, // Ch. 10: The Distant Plains
+    CHAPTER_0C = 0x0C, // Ch. 11: Taking Leave (E)
+    CHAPTER_0D = 0x0D, // Ch. 11: Another Journey (H)
+    CHAPTER_0E = 0x0E, // Ch. 12: Birds of a Feather
+    CHAPTER_0F = 0x0F, // Ch. 13: In Search of Truth
+    CHAPTER_10 = 0x10, // Ch. 13x: The Peddler Merlinus
+    CHAPTER_11 = 0x11, // Ch. 14: False Friends
+    CHAPTER_12 = 0x12, // Ch. 15: Talons Alight (H)
+    CHAPTER_13 = 0x13, // Ch. 15/16: Noble Lady of Caelin
+    CHAPTER_14 = 0x14, // Ch. 16/17: Whereabouts Unknown
+    CHAPTER_15 = 0x15, // Ch. 16x/17x: The Port of Badon
+    CHAPTER_16 = 0x16, // Ch. 17/18: Pirate Ship
+    CHAPTER_17 = 0x17, // Ch. 18/19: The Dread Isle
+    CHAPTER_18 = 0x18, // Ch. 18x/19x: Imprisoner of Magic
+    CHAPTER_19 = 0x19, // Ch. 19x pt2: A Glimpse in Time (H)
+    CHAPTER_1A = 0x1A, // Ch. 19/20: Dragon's Gate
+    CHAPTER_1B = 0x1B, // Ch. 20/21: New Resolve
+    CHAPTER_1C = 0x1C, // Ch. 21/22: Kinship's Bond
+    CHAPTER_1D = 0x1D, // Ch. 22/23: Living Legend
+    CHAPTER_1E = 0x1E, // Ch. 22x/23x: Genesis
+    CHAPTER_1F = 0x1F, // Ch. 23/24: Four-Fanged Offense (Lloyd)
+    CHAPTER_20 = 0x20, // Ch. 23/24: Four-Fanged Offense (Linus)
+    CHAPTER_21 = 0x21, // Ch. 25: Crazed Beast (H)
+    CHAPTER_22 = 0x22, // Ch. 24/26: Unfulfilled Heart
+    CHAPTER_23 = 0x23, // Ch. 25/27: Pale Flower of Darkness (Kenneth)
+    CHAPTER_24 = 0x24, // Ch. 25/27: Pale Flower of Darkness (Jerme)
+    CHAPTER_25 = 0x25, // Ch. 26/28: Battle before Dawn
+    CHAPTER_26 = 0x26, // Ch. 26x/28x: Night of Farewells
+    CHAPTER_27 = 0x27, // Ch. 27/29: Cog of Destiny
+    CHAPTER_28 = 0x28, // Ch. 28: Valorous Roland (E)
+    CHAPTER_29 = 0x29, // Ch. 30: The Berserker (H)
+    CHAPTER_2A = 0x2A, // Ch. 29/31: Sands of Time
+    CHAPTER_2B = 0x2B, // Ch. 29x/31x: Battle Preparations
+    CHAPTER_2C = 0x2C, // Ch. 30/32: Victory or Death
+    CHAPTER_2D = 0x2D, // Ch. 32x: The Value of Life (H)
+    CHAPTER_2E = 0x2E, // Final Chapter: Light (pt1)
+    CHAPTER_2F = 0x2F, // Final Chapter: Light (pt2)
+};
